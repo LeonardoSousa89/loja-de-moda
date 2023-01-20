552e407b3dcab59fab91f0016abac83f9acb383d
Mendes Sousa <leoprofessionallogytech@outlook.com>
2023-01-20 15:56:22 -0300

    nova_versao

---
+++ src/main/java/com/moda/loja/tendencia/TendenciaApplication.java
@@ -0,0 +1,13 @@
+package com.moda.loja.tendencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TendenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TendenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moda/loja/tendencia/TendenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moda.loja.tendencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TendenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
